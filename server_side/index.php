bbd91a842c882b2917114fa2354f44da02e9bd30
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-09-15 09:05:52 +0200

    structure with foreach to create card

--- server_side/index.php
+++ server_side/index.php
@@ -1,3 +1,57 @@
+<?php
+    $dischi = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'Brave new World',
+            'author' => 'Iron Maiden',
+            'year' => 2000,
+            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
+        ]
+    ];
+
+
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,63 +70,20 @@
     <main>
         <div class="cards_container">
 
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
+        <?php
+            foreach ( $dischi as $disc){
+                echo'<div class="disc_card">';
+                echo'<div class="image_disc">'.$disc['poster'].'</div>';
+                echo'<div class="title_disc">'.$disc['title'].'</div>';
+                echo'<div class="author_disc">'.$disc['author'].'</div>';
+                '</div>'
+            }
 
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
 
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
+        ?>
 
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
-
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
-
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
-
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
-
-            <div class="disc_card">
-                <div class="image_disc">img</div>
-                <div class="title_disc">title</div>
-                <div class="author_disc">author</div>
-            </div>
+             
+        
 
         </div>
 
